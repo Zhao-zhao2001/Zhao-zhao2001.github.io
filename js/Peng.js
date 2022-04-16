409cba19dcd41effb3917d1810c4482c04cd7a2e
Zhao-zhao2001 <1524276984@qq.com>
2022-04-16 21:44:14 +0800

    Site updated: 2022-04-16 21:44:06

--- js/Peng.js
+++ js/Peng.js
@@ -1,5 +1,5 @@
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://gitee.com/Zhao-zhao2001/images/raw/master/img/up.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://changpeng-image.oss-cn-hangzhou.aliyuncs.com/images-master/img/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 // 可爱的Title
